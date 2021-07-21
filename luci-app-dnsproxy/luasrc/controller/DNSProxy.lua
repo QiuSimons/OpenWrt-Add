4c9270bfd0428dabdeacfae8bcda0b57d44e5083
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-07-21 21:45:41 +0800

    Add files via upload

---
+++ luci-app-dnsproxy/luasrc/controller/DNSProxy.lua
@@ -0,0 +1,15 @@
+module("luci.controller.DNSProxy",package.seeall)
+function index()
+if not nixio.fs.access("/etc/config/DNSProxy")then
+return
+end
+	entry({"admin","services","DNSProxy"},cbi("DNSProxy"),_("DNSProxy"),30).dependent=true
+    entry({"admin","services","DNSProxy","status"},call("act_status")).leaf=true
+end 
+
+function act_status()
+  local e={}
+  e.running=luci.sys.call("pgrep -f 5335 >/dev/null")==0
+  luci.http.prepare_content("application/json")
+  luci.http.write_json(e)
+end
