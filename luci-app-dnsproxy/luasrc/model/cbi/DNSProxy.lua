53a63a6fdc58353189002a86289300cafce98995
msylgj <msylgj@vip.qq.com>
2021-07-22 23:58:22 +0800

    mod luci (#4)
    Co-authored-by: QiuSimons <45143996+QiuSimons@users.noreply.github.com>

--- luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
+++ luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
@@ -1,10 +1,3 @@
-require("luci.sys")
-require("luci.util")
-require("luci.http")
-require("luci.dispatcher")
-require("nixio.fs")
-local fs=require"nixio.fs"
-
 mp = Map("DNSProxy", translate("DNSProxy"))
 mp.description = translate("DNSProxy is a Simple DNS proxy with DoH, DoT, DoQ and DNSCrypt support.")
 mp:section(SimpleSection).template  = "DNSProxy/DNSProxy_status"
@@ -17,19 +10,27 @@ enabled = s:option(Flag, "enabled", translate("Enable"))
 enabled.default = 0
 enabled.rmempty = false
 
-node5335 = s:option(Value, "node5335", translate("Node 5335"))
-node5335.default = "-u tls://8.8.4.4:853 -u tls://1.1.1.1:853 --cache --cache-min-ttl=3600 --fastest-addr --ipv6-disabled"
-node5335.placeholder = "-u tls://8.8.4.4:853 -u tls://1.1.1.1:853 --cache --cache-min-ttl=3600 --fastest-addr --ipv6-disabled"
-node5335.datatype = "string"
+oversea_port = s:option(Value, "oversea_port", translate("DNSProxy Port1"))
+oversea_port.default = "5335"
+oversea_port.datatype = "port"
+oversea_port.rmempty = false
 
-node6050 = s:option(Value, "node6050", translate("Node 6050"))
-node6050.default = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
-node6050.placeholder = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
-node6050.datatype = "string"
+oversea_dns = s:option(DynamicList, "oversea_dns", translate("Upsteam DNS"))
+oversea_dns.description = translate("Upsteam DNS Server For DNSProxy Port1 (Support DoT/DoH)")
+oversea_dns.rmempty = true
 
-local apply = luci.http.formvalue("cbi.apply")
-if apply then
-    io.popen("/etc/init.d/DNSProxy reload &")
-end
+oversea_noipv6 = s:option(Flag, "oversea_noipv6", translate("Filter AAAA"))
+oversea_noipv6.default = 1
+oversea_noipv6.rmempty = false
+oversea_noipv6.description = translate("DNSProxy Port1 Filter AAAA Result")
+
+mainland_port = s:option(Value, "mainland_port", translate("DNSProxy Port2"))
+mainland_port.default = "6050"
+mainland_port.datatype = "port"
+mainland_port.rmempty = false
+
+mainland_dns = s:option(DynamicList, "mainland_dns", translate("Upsteam DNS"))
+mainland_dns.description = translate("Upsteam DNS Server For DNSProxy Port2 (Support DoT/DoH)")
+mainland_dns.rmempty = true
 
 return mp
