612e7e7a89a25c9c903b47e1db9ca8af79e9c26f
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-07-22 12:44:19 +0800

    Luci dnsproxy next (#2)
    * Update DNSProxy.lua
    
    * Update DNSProxy
    
    * Update DNSProxy.lua
    
    * Update DNSProxy.lua
    
    * Update dnsproxy.po
    
    * Update DNSProxy

--- luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
+++ luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
@@ -4,8 +4,6 @@ require("luci.http")
 require("luci.dispatcher")
 require("nixio.fs")
 local fs=require"nixio.fs"
-local port=require"luci.model.uci".cursor()
-local port=port:get("DNSProxy","DNSProxy","port")
 
 mp = Map("DNSProxy", translate("DNSProxy"))
 mp.description = translate("DNSProxy is a Simple DNS proxy with DoH, DoT, DoQ and DNSCrypt support.")
@@ -19,21 +17,15 @@ enabled = s:option(Flag, "enabled", translate("Enable"))
 enabled.default = 0
 enabled.rmempty = false
 
--- manual-config
-addr = s:option(Value, "manual-config", translate("Manual Configuration"),
-translate("------------------------------------------------------------------------------------------------------------"))
+node5335 = s:option(Value, "node5335", translate("Node 5335"))
+node5335.default = "-u tls://8.8.4.4:853 -u tls://1.1.1.1:853 --cache --cache-min-ttl=3600 --fastest-addr --ipv6-disabled"
+node5335.placeholder = "-u tls://8.8.4.4:853 -u tls://1.1.1.1:853 --cache --cache-min-ttl=3600 --fastest-addr --ipv6-disabled"
+node5335.datatype = "string"
 
-addr.template = "cbi/tvalue"
-addr.rows = 25
-
-function addr.cfgvalue(self, section)
-	return nixio.fs.readfile("/etc/init.d/DNSProxy")
-end
-
-function addr.write(self, section, value)
-	value = value:gsub("\r\n?", "\n")
-	nixio.fs.writefile("/etc/init.d/DNSProxy", value)
-end
+node6050 = s:option(Value, "node6050", translate("Node 6050"))
+node6050.default = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
+node6050.placeholder = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
+node6050.datatype = "string"
 
 local apply = luci.http.formvalue("cbi.apply")
  if apply then
