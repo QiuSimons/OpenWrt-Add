3a90f3a9cf4f6fba6ec16fa91691968f841c265b
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-07-22 12:40:25 +0800

    Update DNSProxy.lua

--- luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
+++ luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
@@ -4,8 +4,6 @@ require("luci.http")
 require("luci.dispatcher")
 require("nixio.fs")
 local fs=require"nixio.fs"
-local port=require"luci.model.uci".cursor()
-local port=port:get("DNSProxy","DNSProxy","port")
 
 mp = Map("DNSProxy", translate("DNSProxy"))
 mp.description = translate("DNSProxy is a Simple DNS proxy with DoH, DoT, DoQ and DNSCrypt support.")
@@ -28,22 +26,6 @@ node6050 = s:option(Value, "node6050", translate("Node 6050"))
 node6050.default = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
 node6050.placeholder = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
 node6050.datatype = "string"
-
--- manual-config
-addr = s:option(Value, "manual-config", translate("Manual Configuration"),
-translate("------------------------------------------------------------------------------------------------------------"))
-
-addr.template = "cbi/tvalue"
-addr.rows = 25
-
-function addr.cfgvalue(self, section)
-	return nixio.fs.readfile("/etc/init.d/DNSProxy")
-end
-
-function addr.write(self, section, value)
-	value = value:gsub("\r\n?", "\n")
-	nixio.fs.writefile("/etc/init.d/DNSProxy", value)
-end
 
 local apply = luci.http.formvalue("cbi.apply")
  if apply then
