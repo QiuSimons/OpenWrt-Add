dd52030a6d96908941c9b660cf576d88eee9c9f6
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-07-22 12:26:11 +0800

    Update DNSProxy.lua

--- luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
+++ luci-app-dnsproxy/luasrc/model/cbi/DNSProxy.lua
@@ -19,6 +19,16 @@ enabled = s:option(Flag, "enabled", translate("Enable"))
 enabled.default = 0
 enabled.rmempty = false
 
+node5335 = s:option(Value, "node5335", translate("Node 5335"))
+node5335.default = "-u tls://8.8.4.4:853 -u tls://1.1.1.1:853 --cache --cache-min-ttl=3600 --fastest-addr --ipv6-disabled"
+node5335.placeholder = "-u tls://8.8.4.4:853 -u tls://1.1.1.1:853 --cache --cache-min-ttl=3600 --fastest-addr --ipv6-disabled"
+node5335.datatype = "string"
+
+node6050 = s:option(Value, "node6050", translate("Node 6050"))
+node6050.default = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
+node6050.placeholder = "-u 119.29.29.29 -u 223.5.5.5 -u 180.76.76.76 --cache --cache-min-ttl=3600 --fastest-addr"
+node6050.datatype = "string"
+
 -- manual-config
 addr = s:option(Value, "manual-config", translate("Manual Configuration"),
 translate("------------------------------------------------------------------------------------------------------------"))
