0ff9d6a9b962fb848d4aca446937fe6a4ed3415c
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-12-11 15:10:23 +0000

    luci-app-cpulimit: tidy up code

--- luci-app-cpulimit/luasrc/controller/cpulimit.lua
+++ luci-app-cpulimit/luasrc/controller/cpulimit.lua
@@ -1,13 +1,12 @@
-
 module("luci.controller.cpulimit", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/cpulimit") then
 		return
 	end
-	
-	local page = entry({"admin", "system", "cpulimit"}, cbi("cpulimit"), luci.i18n.translate("cpulimit"), 65)
-	page.i18n = "cpulimit"
+
+	local page = entry({"admin", "system", "cpulimit"}, cbi("cpulimit"), _("cpulimit"), 65)
 	page.dependent = true
+	page.acl_depends = { "luci-app-cpulimit" }
 	
 end
