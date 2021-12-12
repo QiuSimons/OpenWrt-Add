239b9baa298e7b4a154a9e24cfd06f336f2fd17c
Beginner <70857188+Beginner-Go@users.noreply.github.com>
2021-12-12 10:36:04 +0800

    luci-app-control-weburl: tidy up code (#14)

--- luci-app-control-weburl/luasrc/controller/weburl.lua
+++ luci-app-control-weburl/luasrc/controller/weburl.lua
@@ -1,10 +1,21 @@
 module("luci.controller.weburl", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/weburl") then return end
+	if not nixio.fs.access("/etc/config/weburl") then
+		return
+	end
 
-    entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
-    entry({"admin", "control", "weburl"}, cbi("weburl"), _("过滤军刀"), 12).dependent =
-        true
+	local page = entry({"admin", "control"}, firstchild(), "Control", 50)
+	page.dependent = false
+	page.acl_depends = { "luci-app-control-weburl" }
+
+	entry({"admin", "control", "weburl"}, cbi("weburl"), _("Weburl"), 12).dependent = true
+	entry({"admin", "control", "weburl", "status"}, call("act_status")).leaf = true
 end
 
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("iptables -L FORWARD|grep WEBURL >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
+end
