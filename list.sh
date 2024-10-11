c402ba10579f204fccbe793c03c815bbcbffab19
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2024-10-11 13:08:22 +0800

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # From: QiuSimons
 
-echo '
-teamviewer.com
+echo 'teamviewer.com
 epicgames.com
 dangdang.com
 account.synology.com
@@ -12,19 +11,16 @@ checkip.dyndns.org
 checkipv6.synology.com
 ntp.aliyun.com
 cn.ntp.org.cn
-ntp.ntsc.ac.cn
-' >>./openwrt_helloworld/luci-app-passwall/root/usr/share/passwall/rules/direct_host
+ntp.ntsc.ac.cn' >>./openwrt_helloworld/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 
 
-echo '
-account.synology.com
+echo 'account.synology.com
 ddns.synology.com
 checkip.synology.com
 checkip.dyndns.org
 checkipv6.synology.com
 ntp.aliyun.com
 cn.ntp.org.cn
-ntp.ntsc.ac.cn
-' >>./luci-app-mosdns/luci-app-mosdns/root/etc/mosdns/rule/whitelist.txt
+ntp.ntsc.ac.cn' >>./luci-app-mosdns/luci-app-mosdns/root/etc/mosdns/rule/whitelist.txt
 
 exit 0
