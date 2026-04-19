d82d724ecba62858a29dc4e397e6ba51693b1a38
SimonsQiu <simonsqiu@foxmail.com>
2026-04-19 16:26:47 +0000

    Update 2026/04/19 16:26:47 UTC

---
+++ openwrt_helloworld/luci-app-momo/htdocs/luci-static/resources/view/momo/log.js
@@ -0,0 +1,147 @@
+'use strict';
+'require form';
+'require view';
+'require uci';
+'require fs';
+'require poll';
+'require tools.momo as momo';
+
+return view.extend({
+    load: function () {
+        return Promise.all([
+            uci.load('momo'),
+            momo.getPaths(),
+            momo.getAppLog(),
+            momo.getCoreLog()
+        ]);
+    },
+    render: function (data) {
+        const paths = data[1];
+        const appLog = data[2];
+        const coreLog = data[3];
+
+        let m, s, o;
+
+        m = new form.Map('momo');
+
+        s = m.section(form.NamedSection, 'log', 'log', _('Log'));
+
+        s.tab('log_config', _('Log Config'));
+        
+        o = s.taboption('log_config', form.Flag, 'scheduled_clear', _('Scheduled Clear'));
+        o.rmempty = false;
+        
+        o = s.taboption('log_config', form.Value, 'scheduled_clear_cron', _('Scheduled Clear Cron'));
+        o.retain = true;
+        o.rmempty = false;
+        o.depends('scheduled_clear', '1');
+
+        o = s.taboption('log_config', form.Value, 'scheduled_clear_size_limit', _('Scheduled Clear Size Limit'));
+        o.retain = true;
+        o.rmempty = false;
+        o.datatype = 'uinteger';
+        o.depends('scheduled_clear', '1');
+
+        o = s.taboption('log_config', form.ListValue, 'scheduled_clear_size_limit_unit', _('Scheduled Clear Size Limit Unit'));
+        o.retain = true;
+        o.rmempty = false;
+        o.depends('scheduled_clear', '1');
+        o.value('KB', 'KB');
+        o.value('MB', 'MB');
+        o.value('GB', 'GB');
+
+        s.tab('app_log', _('App Log'));
+
+        o = s.taboption('app_log', form.Button, 'clear_app_log');
+        o.inputstyle = 'negative';
+        o.inputtitle = _('Clear Log');
+        o.onclick = function (_, section_id) {
+            m.lookupOption('_app_log', section_id)[0].getUIElement(section_id).setValue('');
+            return momo.clearAppLog();
+        };
+
+        o = s.taboption('app_log', form.TextValue, '_app_log');
+        o.rows = 25;
+        o.wrap = false;
+        o.load = function (section_id) {
+            return appLog;
+        };
+        o.write = function (section_id, formvalue) {
+            return true;
+        };
+        poll.add(L.bind(function () {
+            const option = this;
+            return L.resolveDefault(momo.getAppLog()).then(function (log) {
+                option.getUIElement('log').setValue(log);
+            });
+        }, o));
+
+        o = s.taboption('app_log', form.Button, 'scroll_app_log_to_bottom');
+        o.inputtitle = _('Scroll To Bottom');
+        o.onclick = function (_, section_id) {
+            const element = m.lookupOption('_app_log', section_id)[0].getUIElement(section_id).node.firstChild;
+            element.scrollTop = element.scrollHeight;
+        };
+
+        s.tab('core_log', _('Core Log'));
+
+        o = s.taboption('core_log', form.Button, 'clear_core_log');
+        o.inputstyle = 'negative';
+        o.inputtitle = _('Clear Log');
+        o.onclick = function (_, section_id) {
+            m.lookupOption('_core_log', section_id)[0].getUIElement(section_id).setValue('');
+            return momo.clearCoreLog();
+        };
+
+        o = s.taboption('core_log', form.TextValue, '_core_log');
+        o.rows = 25;
+        o.wrap = false;
+        o.load = function (section_id) {
+            return coreLog;
+        };
+        o.write = function (section_id, formvalue) {
+            return true;
+        };
+        poll.add(L.bind(function () {
+            const option = this;
+            return L.resolveDefault(momo.getCoreLog()).then(function (log) {
+                option.getUIElement('log').setValue(log);
+            });
+        }, o));
+
+        o = s.taboption('core_log', form.Button, 'scroll_core_log_to_bottom');
+        o.inputtitle = _('Scroll To Bottom');
+        o.onclick = function (_, section_id) {
+            const element = m.lookupOption('_core_log', section_id)[0].getUIElement(section_id).node.firstChild;
+            element.scrollTop = element.scrollHeight;
+        };
+
+        s.tab('debug_log', _('Debug Log'));
+
+        o = s.taboption('debug_log', form.Button, '_generate_download_debug_log');
+        o.inputstyle = 'negative';
+        o.inputtitle = _('Generate & Download');
+        o.onclick = function () {
+            return momo.debug().then(function () {
+                fs.read_direct(paths.debug_log_path, 'blob').then(function (data) {
+                    // create url
+                    const url = window.URL.createObjectURL(data, { type: 'text/markdown' });
+                    // create link
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.download = 'debug.log';
+                    // append to body
+                    document.body.appendChild(link);
+                    // download
+                    link.click();
+                    // remove from body
+                    document.body.removeChild(link);
+                    // revoke url
+                    window.URL.revokeObjectURL(url);
+                });
+            });
+        };
+
+        return m.render();
+    }
+});
---
+++ openwrt_helloworld/momo/files/uci-defaults/migrate.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+. "$IPKG_INSTROOT/etc/momo/scripts/include.sh"
+
+# since v1.0.2
+
+section_placeholder=$(uci -q get momo.placeholder); [ -z "$section_placeholder" ] && uci set momo.placeholder="placeholder"
+
+# since v1.1.0
+
+proxy_bypass_china_mainland_ip=$(uci -q get momo.proxy.bypass_china_mainland_ip)
+proxy_bypass_china_mainland_ip6=$(uci -q get momo.proxy.bypass_china_mainland_ip6)
+[ -z "$proxy_bypass_china_mainland_ip6" ] && uci set momo.proxy.bypass_china_mainland_ip6=$proxy_bypass_china_mainland_ip
+
+routing_tproxy_fw_mask=$(uci -q get momo.routing.tproxy_fw_mask); [ -z "$routing_tproxy_fw_mask" ] && uci set momo.routing.tproxy_fw_mask=0xFF
+routing_tun_fw_mask=$(uci -q get momo.routing.tun_fw_mask); [ -z "$routing_tun_fw_mask" ] && uci set momo.routing.tun_fw_mask=0xFF
+
+procd=$(uci -q get momo.procd); [ -z "$procd" ] && {
+	uci set momo.procd=procd
+	uci set momo.procd.fast_reload=$(uci -q get momo.config.fast_reload)
+	uci del momo.config.fast_reload
+}
+
+# since v1.1.1
+
+dummy_device=$(uci -q get momo.routing.dummy_device); [ -z "$dummy_device" ] && uci set momo.routing.dummy_device=momo-dummy
+
+# since v1.1.2
+
+config_scheduled_restart_cron=$(uci -q get momo.config.scheduled_restart_cron); [ -z "$config_scheduled_restart_cron" ] && uci rename momo.config.cron_expression="scheduled_restart_cron"
+
+section_log=$(uci -q get momo.log); [ -z "$section_log" ] && {
+	uci set momo.log=log
+	uci set momo.log.scheduled_clear=1
+	uci set momo.log.scheduled_clear_cron="*/5 * * * *"
+	uci set momo.log.scheduled_clear_size_limit=1
+	uci set momo.log.scheduled_clear_size_limit_unit=MB
+}
+
+section_mixin=$(uci -q get momo.mixin); [ -z "$section_mixin" ] && {
+	uci set momo.mixin=mixin
+	uci set momo.mixin.log_disabled='0'
+	uci set momo.mixin.log_level='info'
+	uci set momo.mixin.log_timestamp='1'
+	uci set momo.mixin.dns_independent_cache='1'
+	uci set momo.mixin.dns_reverse_mapping='1'
+	uci set momo.mixin.cache_enabled='1'
+	uci set momo.mixin.cache_store_fakeip='1'
+	uci set momo.mixin.cache_store_rdrc='1'
+	uci set momo.mixin.external_control_ui_path='ui'
+	uci set momo.mixin.external_control_ui_download_url='https://github.com/Zephyruso/zashboard/releases/latest/download/dist-cdn-fonts.zip'
+}
+
+# commit
+uci commit momo
+
+# exit with 0
+exit 0
