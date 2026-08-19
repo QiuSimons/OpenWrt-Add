edfd2d540cb8218bf31e15a27e4bbfd42857710a
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2026-08-19 14:14:06 +0800

    Delete openwrt-honk directory

--- openwrt-honk/honk/source.mk
+++
@@ -1,5 +0,0 @@
-# Generated from locks/source.lock.json. Do not edit.
-PKG_SOURCE_VERSION:=88759842126ef9ca8431af50923220084f6f7cdd
-PKG_SOURCE:=88759842126ef9ca8431af50923220084f6f7cdd.tar.gz
-PKG_SOURCE_URL:=https://github.com/Glassyiris/honk/archive
-PKG_MIRROR_HASH:=skip
--- openwrt-honk/luci-app-honk/ui/src/page-actions.ts
+++
@@ -1,7 +0,0 @@
-export type PageAction = {
-  id: string
-  label: string
-  disabled?: boolean
-  busy?: boolean
-  run: () => void | Promise<void>
-}
